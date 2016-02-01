babaa70828893be9ce3eb4ccab88d725596c2e4b
neilsimp1 <neilsimp1@gmail.com>
2016-02-01 15:21:19 -0500

    Added jQuery and started script file

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	
+	//login enable/disable
+	$('#un, #pw').on('change', function(){
+		if($I('un').value === '' && $I('un').value === '') $I('login-submit').disabled = true;
+		else $I('login-submit').disabled = false;
+	});
+	
+	
+};
+
+
+
+
+
+function $I(i){return document.getElementById(i);}
